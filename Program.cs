6fcdf269b21d28fe2b9c484f3d3776862a8fe236
“shbhmkmr987” <shbhmkmr987@gmail.com>
2018-12-30 21:48:41 +0530

    Adding Program.cs file

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+    class Program
+    {
+        static void bank(float p, float r , float t){
+            float i = p*r*t/100;
+            Console.WriteLine($"The interest is {i}");
+            float a = p+i;
+            Console.WriteLine($"The amount is {a}");
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine($"the i is {i} and the  a is {a}");
+        }
+        static void Main(string[] args)
+        {
+            float rate ;
+            Console.WriteLine("Enter  principal:");
+            float principal = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter time in years:");
+            float time = float.Parse(Console.ReadLine());
+
+            if(time <= 5)
+                rate = 4;
+            else 
+                rate = 5;
+
+            bank(principal,rate,time);
+        }
+    }
+}
